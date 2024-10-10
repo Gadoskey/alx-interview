9519df8c573355154692e5049ba57dc756962fc5
Gadoskey <mustopha.yusufope@gmail.com>
2024-10-10 13:21:46 +0000

    pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,26 +6,25 @@ Function: canUnlockAll
 Description: This function determines
   if all boxes in a list of lists can be unlocked.
 Each box may contain keys to other boxes.
-We start with box 0 unlocked and use the keys 
+We start with box 0 unlocked and use the key
   found inside boxes to open others.
 The function returns True if all boxes can be opened,
   else returns False.
 """
 
+
 def canUnlockAll(boxes):
+
     # Start with box 0 unlocked
     unlocked = [False] * len(boxes)
     unlocked[0] = True  # Box 0 is always unlocked
-    
     # A list of keys you currently have, starting with keys in box 0
     keys = boxes[0]
-    
     # Check which boxes can be unlocked with the keys you find
     for key in keys:
         if key < len(boxes) and not unlocked[key]:
             unlocked[key] = True
             # Add new keys from the newly unlocked box
             keys.extend(boxes[key])
-    
     # Return True if all boxes are unlocked
     return all(unlocked)
