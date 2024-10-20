ca16d922fe87f0fed49716f13f744d5bb9baad90
Gadoskey <gadoski321@gmail.com>
2024-10-20 15:00:36 +0100

    solved minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Author: Yusuf Mustapha Opeyemi
+File: 0-minoperations.py
+Function: minOperations
+Description: This function creates the pascal triangle
+"""
+
+
+def minOperations(n: int) -> int:
+    # If n is less than or equal to 1, return 0 as it's impossible
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            # If divisible, add the divisor to operations
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
