65d2a6580b8a9ce1f9713d1f3c3946b9d7de7172
Gadoskey <gadoski321@gmail.com>
2024-11-16 10:55:57 +0100

    testing

---
+++ 0x06-starwars_api/webpage_header_checker.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+/*
+  Author: Yusuf Mustapha Opeyemi
+  Desc: Writing a script that checks the return type of a webpage
+  
+  Still Playing with API
+
+*/
+
+const request = require("request");
+
+request("https://example.com/api", (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  if ((response.statusCode !== 200)) {
+    console.errro("Error fetching details");
+  }
+  console.log(response.headers["content-type"]);
+  console.log(body);
+});
