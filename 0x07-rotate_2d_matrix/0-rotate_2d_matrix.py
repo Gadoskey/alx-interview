ea960c992a5892c0b5e4db0a89c8b6894c7d2daa
Gadoskey <gadoski321@gmail.com>
2024-11-19 13:45:49 +0100

    removed whitespaces

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,7 +8,7 @@
 """Steps:
     Step 1: Transpose the matrix -
 
-      The transpose of a matrix is an operation that flips 
+      The transpose of a matrix is an operation that flips
       the matrix over its diagonal. Specifically, for a given matrix,
       it converts the row indices to column indices and vice versa.
 
@@ -20,17 +20,17 @@
 
 
 
-      In simpler terms: 
+      In simpler terms:
         The first row becomes the first column.
         The second row becomes the second column, and so on.
 
        [[1 2 3]          [[1 4 7]
-        
+
         [4 5 6]   --->    [2 5 8]
-        
+
         [7 8 9]]          [3 6 9]]
 
-    Step 2: Reverse each row - 
+    Step 2: Reverse each row -
       [1, 4, 7] becomes [7, 4, 1]
       [2, 5, 8] becomes [8, 5, 2]
       [3, 6, 9] becomes [9, 6, 3]
@@ -40,20 +40,21 @@
 def rotate_2d_matrix(matrix):
     """
     Rotates an n x n 2D matrix 90 degrees clockwise in-place.
-    
+
     Arg:
         matrix: List[List[int]], the matrix to rotate
     """
     n = len(matrix)
-    
+
     # Transpose the matrix
     for i in range(n):
         for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     # Reverse each row
     for row in matrix:
         row.reverse()
+
 
 """
   How TRANSPOSE Helps in Rotation:
