73de25d5eed0716fe1914155e59e4c58d36c9712
Gadoskey <gadoski321@gmail.com>
2024-11-19 13:52:34 +0100

    okkk

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -46,7 +46,8 @@ def rotate_2d_matrix(matrix):
     """
     n = len(matrix)
 
-    # Transpose the matrix
+    # Transpose the matrix manually
+    # In other instances you can use numpy though, Quite easier to use
     for i in range(n):
         for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
